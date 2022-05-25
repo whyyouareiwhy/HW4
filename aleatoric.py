2ddc800d2cb768867688fb2842377907aa5723b8
Yuriy Alexander <yuriy@Mac.local>
2022-05-24 22:01:29 -0700

    Added random music generation with square/sine notes

--- aleatoric.py
+++ aleatoric.py
@@ -1,54 +1,78 @@
 import numpy as np
+import scipy.signal
 import simpleaudio as sa
 import scipy.io.wavfile as wav
 import time
-import sys
-import getopt
 import argparse
+import random
+
+SAMPLERATE = 48000
+DURATION = 0.5
 
 
-def sin_wave(freq):
-    frequency = freq         # Hz (cycles/second)
-    sample_rate = 48000     # samples/second
-    duration = 0.5            # seconds
-
-    t = np.linspace(0, duration, int(duration * sample_rate), False)
-    sine_note = np.sin(frequency * t * (2 * np.pi))
+def sin_wave(freq, amp, clip):
+    t = np.linspace(0, DURATION, int(DURATION * SAMPLERATE), False)
+    sine_note = np.sin(freq * t * (2 * np.pi))
     audio = sine_note * (2 ** 15 - 1) / np.max(np.abs(sine_note))
 
-    # scale audio to 1/2 amplitude
-    amplitude = 10 ** (-6 / 20)
-    audio *= amplitude
+    # root note amplitude = --accent and non-root notes = --volume
+    audio *= (amp/10)
+
+    # Square the wave on the root note if clip arg is True
+    if clip:
+        audio = np.clip(audio, a_min=-8192, a_max=8192)
+        wav.write('clipped.wav', SAMPLERATE, audio)
     audio = audio.astype(np.int16)
-
-    # clip amplitude over/under 1/4
-    # clipped_audio = np.clip(audio, -8192, 8192)
-
-    # write to wav file (can remove later!!!)
-    # wav.write('clipped.wav', sample_rate, clipped_audio)
-
     return audio
 
 
+def square_wave(freq, amp):
+    t = np.linspace(0, 1, 1000, endpoint=True)
+    # audio = scipy.signal.square(2 * np.pi * SAMPLERATE * t)
+    # audio = audio.astype(np.int16)
+    return t
+
+
+# Play a given measure in order from C base note to next octave's C
 def play_measure(octave_notes):
     for note in major_scale_notes:
         pitch = octave_notes[note]
-        audio_sample = sin_wave(pitch)
-        # Play using audio buffer
-        wave_obj = sa.WaveObject(audio_sample, 1, 2, 48000)
+        audio_sample = sin_wave(pitch, options.accent)
+        wave_obj = sa.WaveObject(audio_sample, 1, 2, SAMPLERATE)
         play_obj = wave_obj.play()
         play_obj.wait_done()
 
 
 # generate music in C[root] measure, root = 2-7
-def random_music(root):
-    print("To Do")
+def random_music(measure):
+    count = 1
+    '''TO-DO - square the root-note wave'''
+    # Play root note with --accent arg settings
+    root = major_notes[0]
+    print(f"{root} / {format(measure[root], '.2f')} / beat {count}")
+    count += 1
+    root_note = sin_wave(measure[root], options.accent, True)
+    wave_obj = sa.WaveObject(root_note, 1, 2, SAMPLERATE)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+    # play (--beats - 1) random notes from major scale
+    for i in range(1, options.beats):
+        rand_note = random.choice(major_notes)
+        print(f"{rand_note} / {format(measure[rand_note], '.2f')} / beat {count}")
+        count += 1
+        non_root_note = sin_wave(measure[rand_note], options.volume, False)
+        wave_obj = sa.WaveObject(non_root_note, 1, 2, SAMPLERATE)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+    print("---------------------------------------------------")
 
 
-octave = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-major_scale = [0, 2, 4, 5, 7, 9, 11]
-notes = [octave[i] for i in major_scale]
-major_scale_notes = [octave[0], octave[2], octave[4], octave[5], octave[7], octave[9], octave[11]]
+octave = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', '_C']  # C_ is next octave's C
+major_scale = [0, 2, 4, 5, 7, 9, 11, 12]  # major scale ends on root C note of next octave
+major_notes = [octave[i] for i in major_scale]
+major_scale_notes = [octave[0], octave[2], octave[4], octave[5], octave[7], octave[9], octave[11], octave[12]]
+
 
 '''Create notes for octave 2-8 which will be sampled for the measure to generate random keys
 using a C note as the base frequency.'''
@@ -67,7 +91,22 @@ parser.add_argument('--bpm', dest='bpm', type=float, default=90.0)  # beats per 
 parser.add_argument('--ramp', dest='ramp', type=float, default=0.50)  # attack and release time
 parser.add_argument('--accent', dest='accent', type=float, default=5.0)  # volume for accented 1st beat
 parser.add_argument('--volume', dest='volume', type=float, default=5.0)  # volume for unaccented beats
-args = parser.parse_args()
+options = parser.parse_args()
+
+print(f"--root {options.root} --beats {options.beats} --bpm {options.bpm} --ramp {options.ramp} --accent {options.accent} --volume {options.volume}")
+while True:
+    if options.root == 36:
+        random_music(octave_2_notes)
+    elif options.root == 48:
+        random_music(octave_3_notes)
+    elif options.root == 60:
+        random_music(octave_4_notes)
+    elif options.root == 72:
+        random_music(octave_5_notes)
+    elif options.root == 84:
+        random_music(octave_6_notes)
+    elif options.root == 96:
+        random_music(octave_7_notes)
 
 # wave_obj = sa.WaveObject.from_wave_file('clipped.wav')
 # play_obj = wave_obj.play()
